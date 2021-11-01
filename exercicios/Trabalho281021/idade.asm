fe1ffb9629ae8ca179c01575a2d5084ade379486
Breeze <victoralmeida5@gmail.com>
2021-11-01 18:29:35 -0300

    commit

--- exercicios/Trabalho281021/idade.asm
+++ exercicios/Trabalho281021/idade.asm
@@ -1,56 +1,57 @@
 %include "asm_io.inc"
+
 segment .data
-entrada db "idade: ", 0
-saida1  db "infantil", 0
-saida2 db "juvenil", 0
-saida3 db "adulto", 0
+    entrada db "idade: ", 0
+    saida1  db "infantil", 0
+    saida2 db "juvenil", 0
+    saida3 db "adulto", 0
 
 segment .bss
-input1 resd 1
-input2 resd 2
+    input1 resd 1
 
 segment .text 
     global _asm_main
 
 _asm_main:
     enter 0,0
-    mov eax, entrada
-    call print_string
-    call read_int
-    mov [input1], eax
-    call print_nl
+    mov eax, entrada            ; move string entrada para eax
+    call print_string           ; print string contida em eax
 
-    cmp eax, 10
-    jle infantil
+    call read_int               ; ler valor 
+    mov [input1], eax           ; armazena valor em input1
+    call print_nl               ; quebra de linha
 
-    cmp eax, 17
-    jle juvenil
+    cmp eax, 10                 ; compara eax com 10
+    jle infantil                ; jump jle se vleft <= vright pula para "infantil:"
 
-    cmp eax, 18
-    jge adulto
+    cmp eax, 17                 ; compara eax com 17
+    jle juvenil                 ; jump jle se vleft <= vright pula para "juvenil:" 
+
+    cmp eax, 18                 ; compara eax com 18
+    jge adulto                  ; jump jge se vleft >= vright pula para "adulto:"
     
 infantil:
-    mov eax, saida1
-    call print_string
-    call print_nl
-    jmp fim
+    mov eax, saida1             ; move saida1 para eax
+    call print_string           ; print eax
+    call print_nl               ; quebra de linha 
+    jmp fim                     ; jmp jump incondicional para "fim:"
 
 juvenil:
-    mov eax, saida2
-    call print_string
-    call print_nl
-    jmp fim
+    mov eax, saida2             ; move saida2 para eax
+    call print_string           ; print eax
+    call print_nl               ; quebra de linha
+    jmp fim                     ; jmp jump incondicional para "fim:"
 
 adulto:
-    mov eax, saida3
-    call print_string
-    call print_nl
-    jmp fim
+    mov eax, saida3             ; move saida3 para eax
+    call print_string           ; print eax
+    call print_nl               ; quebra de linha
+    jmp fim                     ; jmp jump incondicional para "fim:"
 
 fim:
-    call print_nl
+    call print_nl               ; quebra de linha
 
-    popa
+    popa                        
     mov eax, 0
     leave
     ret 
