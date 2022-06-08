e8045a69243ce07ae4dd702dea396b37b1726845
Victor Almeida <victordealmeida5@gmail.com>
2022-06-08 16:42:23 -0300

    150908062022

--- 08/main.asm
+++ 08/main.asm
@@ -22,8 +22,10 @@ segment .bss
     input0  resd 1                  ;hora inicio
     input1  resd 1                  ;Minuto inicio
     input2  resd 1                  ;segundos inicio
-    input3  resd 1                  ;duracao inicio
-    input4  resd 1                  
+    input3  resd 1                  ;duracao
+    input4  resd 1                  ;hora final
+    input5  resd 1                  ;minuto final
+    input6  resd 1                  ;segundos final
 
 segment .text
     global _asm_main
@@ -43,7 +45,7 @@ _asm_main:
     call print_string
     
     call read_int                   ;Ler minuto
-    mov [input1], eax
+    mov [input1], eax               ;minuto inicio = eax
     call print_nl
 
     mov eax, entrada2               ;Informe segundos
@@ -57,31 +59,44 @@ _asm_main:
     call print_string
     
     call read_int                   ;Ler duracao
-    mov [input3], eax
+    mov [input3], eax               ;duracao = eax
     call print_nl
 
     mov eax, saida1                 ;informe saida hora
     call print_string
-    mov eax, [input3]               ;hora
+    mov eax, [input3]               ;duracao
     mov ebx, 3600                   ;3600
     mov edx, 0                      ;edx = 0
-    idiv ebx                        ;eax = hora/3600
-    
+    idiv ebx                        ;eax = duracao/3600
+    mov [input4], eax               ;horas = eaxs
+    mov eax, [input0]               ;
+    add eax, [input4]               ;eax = hora inicio + horas duracao
     call print_int
     call print_nl
-    mov [input4], eax               ;
 
-    mov eax, saida2
+    mov eax, saida2                 ;informe saida minutos
     call print_string
-    mov eax, [input4]
-    mov ebx, 3600
-    mul ebx
-    sub eax, [input3]
-    mov ebx, 60
-    mov edx, 0
-    idiv ebx
+    mov eax, [input4]               ;eax = hora
+    mov ebx, 3600                   ;ebx = 3600
+    mul ebx                         ;eax = horas*3600
+    sub eax, [input3]               ;eax = eax - duracao
+    mov ebx, 60                     ;ebx = 60
+    mov edx, 0                      ;edx = 0
+    idiv ebx                        ;eax = eax/ebx
+                                
+    mov ebx, [input1]               ;ebx = minuto inicio
+    add eax, ebx                    ;eax = ebx + eax
+    mov [input5], eax               ;minuto final = eax
     call print_int
     call print_nl
+
+
+    mov eax, saida3
+    call print_string
+    mov eax, [input4]               ;eax = hora
+    mov ebx, 3600                   ;ebx = 3600
+    mul ebx                         ;eax = horas*3600
+    mov ebx, [input5]               ;ebx = minutos
 
 
     jmp Fim
