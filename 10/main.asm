aec0300a29fa230e5fe3d1d27762f5081a46a324
Victor Almeida in work <victordealmeida5@gmail.com>
2022-07-20 08:23:15 -0300

    082320072022

--- 10/main.asm
+++ 10/main.asm
@@ -5,6 +5,9 @@
 ;   gcc -c -o driver.obj driver.c
 ;   gcc -o main driver.obj main.obj asm_io.obj
 
+;	Escreva um programa que leia as coordenadas x e y de pontos no R²
+;	e calcule sua distância da origem (0,0).
+
 segment .data
 
 segment .bss
